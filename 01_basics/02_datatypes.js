22301d92e1aa13bc2f431ca4e034e8d342822ee6
Guptarishabhh <rishabhguptass123@gmail.com>
2026-05-07 02:13:19 +0530

    datatypes concepts

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+// data types in js
+
+let name = "rishabh"
+let age = 23
+
+console.log();
+
+// => primitive datatype
+    // -> string used for text
+    // -> number stores numbers and integers
+    // -> Boolean stores only [true, false]
+    // -> Undefined var declared but value not b assign
+    // -> Null stanalone  value
+    // -> Symbol use to create unique values
+    // -> Bigint use for very large integers 
+
+// => non-Primitive datatypes
+    // -> [object] ->it stores data in key values
+    // -> [Array] -> use to store multiple values
+    // -> [Functions] ->  it is also type of object we need to create function to define value
+
+// if u want to know the type of datatype use //
+
+console.log(typeof "rishabh"); 
+// it gives string
+console.log(typeof age);
+// it gives number
+
+
