5bf7e847c174e9ec6bbc41c38e11cbdfdf4a48fe
Guptarishabhh <rishabhguptass123@gmail.com>
2026-05-09 01:00:02 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "Rishabh"
+const repoCount = 6
+
+// the best way to write nd call variables
+
+// console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const friendName = new String ('rishuji')
+
+// console.log(friendName[0]);
+// console.log(friendName.__proto__);
+
+// console.log(friendName.lenght);
+// console.log(friendName.toUppercase());
+// console.log(friendName.charAt(3));
+// console.log(friendName.indexOf('j'));
+
+// 
+//  const newString = friendName.substring(0, 5)
+//  console.log(newString);
+
+//  Slice method
+//  const anotherString = friendName.slice(-6, 4)
+//  console.log(anotherString);
+
+// Trim method
+//  const newString1 = "     rishabh    "
+//  console.log(newString1);
+//  console.log(newString1.trim());
+
+// Replace method
+const url = "https://rishabh.com/rishabh%20gupta"
+console.log(url.replace('%20', '-'));
+
+ 
+ 
+ 
+
+
